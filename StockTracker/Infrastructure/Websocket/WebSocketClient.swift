2aa5353541335537106dd02b8cc111629c7533b8
Jameel Shehadeh <jamshehadeh@gmail.com>
2026-04-02 13:16:08 +0400

    feat: add WebSocketClient protocol

---
+++ StockTracker/Infrastructure/Websocket/WebSocketClient.swift
@@ -0,0 +1,15 @@
+//
+//  WebSocketClient.swift
+//  StockTracker
+//
+//  Created by Jameel Shehadeh on 02/04/2026.
+//
+
+import Foundation
+
+protocol WebSocketClient {
+    func connect() async throws
+    func disconnect()
+    func send(_ message: String) async throws
+    func receive() -> AsyncStream<String>
+}
