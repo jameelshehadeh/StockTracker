9b26f623d3f311ea17124b5882ca0645768ac951
Jameel Shehadeh <jamshehadeh@gmail.com>
2026-04-02 10:05:01 +0400

    feat: add Stock domain entity

---
+++ StockTracker/Domain/Entities/Stock.swift
@@ -0,0 +1,17 @@
+//
+//  Stock.swift
+//  StockTracker
+//
+//  Created by Jameel Shehadeh on 02/04/2026.
+//
+
+import Foundation
+
+struct Stock: Identifiable {
+    let id: String
+    let symbol: String
+    let name: String
+    let price: Double
+    let priceChange: Double
+    let description: String
+}
