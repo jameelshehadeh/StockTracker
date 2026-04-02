4409c9e07a33066fb894f36a935a8ca64e48720f
Jameel Shehadeh <jamshehadeh@gmail.com>
2026-04-02 10:00:24 +0400

    chore: organize project with clean architecture structure

---
+++ StockTracker/Presentation/StockList/StockListView.swift
@@ -0,0 +1,21 @@
+//
+//  StockListView.swift
+//  StockTracker
+//
+//  Created by Jameel Shehadeh on 02/04/2026.
+//
+
+import SwiftUI
+
+struct StockListView: View {
+    var body: some View {
+        VStack {
+            Text("Stocks")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    StockListView()
+}
