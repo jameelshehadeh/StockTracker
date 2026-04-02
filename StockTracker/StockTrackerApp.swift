c6925992f2bead102df5e2b1d3fc2b4902e7c12e
Jameel Shehadeh <jamshehadeh@gmail.com>
2026-04-02 09:19:53 +0400

    Initial Commit

---
+++ StockTracker/StockTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTrackerApp.swift
+//  StockTracker
+//
+//  Created by Jameel Shehadeh on 02/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct StockTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
