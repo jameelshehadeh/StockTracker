fc74e42ea5cc74652981b76b7b10a38236a82c20
Jameel Shehadeh <jamshehadeh@gmail.com>
2026-04-03 16:22:33 +0400

    refactor: rename ConnectionState to SocketConnectionState and move to infrastructure layer

--- StockTracker/Domain/Repositories/StockRepository.swift
+++ StockTracker/Domain/Repositories/StockRepository.swift
@@ -7,14 +7,8 @@
 
 import Foundation
 
-enum ConnectionState {
-    case connected
-    case disconnected
-}
-
 protocol StockRepository {
     var priceUpdates: AsyncStream<Stock> { get }
-    var connectionState: ConnectionState { get }
     func getStocks() async throws -> [Stock]
     func startPriceFeed() async throws
     func stopPriceFeed()
---
+++ StockTracker/Infrastructure/Websocket/SocketConnectionState.swift
@@ -0,0 +1,12 @@
+//
+//  SocketConnectionState.swift
+//  StockTracker
+//
+//  Created by Jameel Shehadeh on 03/04/2026.
+//
+
+
+enum SocketConnectionState {
+    case connected
+    case disconnected
+}
